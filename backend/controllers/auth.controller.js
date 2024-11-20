2c829fb054b9e9bfbbf9246693c6858f042421ef
chithranjan kumar <kumarchithranjan@gmail.com>
2024-11-20 10:37:38 +0530

    adding login, signup and logout backend

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,84 @@
+import bcrypt from "bcryptjs";
+import { UserModel } from "../models/user.model.js";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res
+        .status(400)
+        .json({ error: "password and confirm password must be same" });
+    }
+
+    const user = await UserModel.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "username already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const defaultProfilePicUrl = `https://avatar.iran.liara.run/public/${
+      gender === "male" ? "boy" : "girl"
+    }?username=${username}`;
+
+    const newUser = await UserModel.create({
+      fullName,
+      username,
+      password: hashedPassword,
+      gender,
+      profilePic: defaultProfilePicUrl,
+    });
+
+    generateTokenAndSetCookie(newUser._id, res);
+
+    res.status(201).json({
+      id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Signup error: ", error);
+    if (error.name === "ValidationError") {
+      return res.status(500).json({ message: error.message });
+    }
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await UserModel.findOne({ username });
+
+    if (!user) {
+      return res.status(400).json({ error: "Invalid username" });
+    }
+
+    const isPassswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPassswordCorrect) {
+      return res.status(400).json({ error: "Incorrect Password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Login error: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("token");
+  res.status(200).json({ message: "Logged out successfully" });
+};
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import authRouter from "./routes/auth.route.js";
+import connectToMongoDB from "./db/connectToMongoDb.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Server listening to port: ${port}`);
+app.listen(port, async () => {
+  try {
+    connectToMongoDB();
+    console.log(`Server listening to port: ${port}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
