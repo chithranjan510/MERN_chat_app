1c08dbf6ea65c9c94811499b3501579fa119737a
chithranjan kumar <kumarchithranjan@gmail.com>
2024-12-10 18:07:33 +0530

    adding loader for image onload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,41 @@ import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import { useAuthContext } from "./context/AuthContext";
+import { useEffect, useState } from "react";
 
 function App() {
   const { userAuthData } = useAuthContext();
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadImages = async () => {
+      const img = new Image();
+      img.src = "/backgroundImage.jpg";
+      try {
+        await new Promise((resolve, reject) => {
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      } catch (error) {
+        console.error(
+          `Failed to load image background image: ${error.message}`
+        );
+      } finally {
+        setImagesLoaded(true);
+      }
+    };
+
+    loadImages();
+  }, []);
+
+  if (!imagesLoaded) {
+    return (
+      <div className="h-svh flex items-center justify-center">
+        <span className="loading loading-spinner loading-lg bg-white" />
+      </div>
+    );
+  }
+
   return (
     <Routes>
       <Route
